05a92c2f0052c1dc332ff1db4a3fc0719de24d99
Ken Allred <zigrivers@gmail.com>
2026-04-12 15:27:47 -0600

    chore: remove stray files from tracking

--- test-types.ts
+++
@@ -1,5 +0,0 @@
-import { WebAppConfigSchema } from './src/config/schema.js'
-import { z } from 'zod'
-type WebAppConfig = z.infer<typeof WebAppConfigSchema>
-const x: WebAppConfig = { renderingStrategy: 'ssr' }
-const y: WebAppConfig = { renderingStrategy: 'ssr', deployTarget: 123 }
--- test-types2.ts
+++
@@ -1,8 +0,0 @@
-import { CliConfigSchema, BackendConfigSchema, LibraryConfigSchema } from './src/config/schema.js'
-import { z } from 'zod'
-type CliConfig = z.infer<typeof CliConfigSchema>
-type BackendConfig = z.infer<typeof BackendConfigSchema>
-type LibraryConfig = z.infer<typeof LibraryConfigSchema>
-const c: CliConfig = { interactivity: 'interactive' }
-const b: BackendConfig = { apiStyle: 'rest' }
-const l: LibraryConfig = { visibility: 'public' }
